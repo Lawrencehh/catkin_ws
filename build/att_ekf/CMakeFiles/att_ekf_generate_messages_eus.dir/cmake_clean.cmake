ade912886d660bb6b6bb30ca850027ea792a9666
Lawrencehh <sy160751@buaa.edu.cn>
2021-02-22 16:13:18 +0800

    test

--- build/att_ekf/CMakeFiles/att_ekf_generate_messages_eus.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "/home/ubuntu/catkin_ws/devel/share/roseus/ros/att_ekf/manifest.l"
-  "CMakeFiles/att_ekf_generate_messages_eus"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/att_ekf_generate_messages_eus.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/att_ekf/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/catkin_ws/src/att_ekf
-# Build directory: /home/ubuntu/catkin_ws/build/att_ekf
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
