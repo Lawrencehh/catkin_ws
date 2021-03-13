8e49f67982f8923f68dd10cbc8ab4bf93034331d
Lawrencehh <sy1607511@buaa.edu.cn>
2021-03-13 15:00:29 +0800

    1

--- src/simple_navigation_goals/src/multi_nav_goals.cpp
+++ src/simple_navigation_goals/src/multi_nav_goals.cpp
@@ -2,6 +2,7 @@
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
 
+
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
 int main(int argc, char** argv){
@@ -17,6 +18,11 @@ int main(int argc, char** argv){
     ROS_INFO("Waiting for the move_base action server to come up");
   }
 
+  double goal_x[] = {3,6,3};
+  double goal_y[] = {7,5,0};
+  double goal_w[] = {M_PI/2,M_PI/2,M_PI/2};
+
+
 
 for(int i =0;i<3;i++){
   move_base_msgs::MoveBaseGoal goal;
@@ -25,18 +31,22 @@ for(int i =0;i<3;i++){
   goal.target_pose.header.frame_id = "base_link";
   goal.target_pose.header.stamp = ros::Time::now();
 
-  goal.target_pose.pose.position.x = 1.0;
-  goal.target_pose.pose.orientation.w = 1.0;
+  goal.target_pose.pose.position.x = goal_x[i];
+  goal.target_pose.pose.position.y = goal_y[i];
+  goal.target_pose.pose.orientation.w = goal_w[i];
 
   ROS_INFO("Sending goal");
+  ROS_INFO_STREAM("goal_x[i]>>>"<< goal_x[i]);
+  ROS_INFO_STREAM("goal_y[i]>>>"<< goal_y[i]);
+  ROS_INFO_STREAM("goal_w[i]>>>"<< goal_w[i]);
   ac.sendGoal(goal);
 
   ac.waitForResult();
 
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    ROS_INFO("Hooray, the base moved 1 meter forward");
+    ROS_INFO("Hooray, got it!!!");
   else
-    ROS_INFO("The base failed to move forward 1 meter for some reason");
+    ROS_INFO("The base failed to reach the goal for some reason");
 
 }
 return 0;
